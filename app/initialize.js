af6fa5aca5b05597233b536cb8266a5140cb0307
Jenny Knuth <jknuth@n.io>
2016-10-23 20:52:42 -0600

    initial setup

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,11 @@
+const ReactDOM = require('react-dom');
+
 window.io = require('socket.io-client');
 
 document.addEventListener('DOMContentLoaded', function init() {
   // do your setup here
+  const App = document.getElementById('app');
+  const dashboard = require('dashboard');
+
+  ReactDOM.render(dashboard, App);
 });
